9e0005cb06266e8f041a6987749b14e6ccc33c80
Naba Kumar Das <nabad600@gmail.com>
2022-06-09 14:30:20 +0530

    user add

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,5 +4,5 @@ mkdir -p /go/myapp
 cd /go/myapp
 go mod init myapp
 go get github.com/kataras/iris/v12@master
-cp -a /app/main.go /go/myapp/main.go
+cp -a /app/main.go /go/myapp/main.go 2> /dev/null
 go run main.go
