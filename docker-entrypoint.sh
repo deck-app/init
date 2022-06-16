388dd38bd8f0ef0edc4dd9d01ce0a3cd0eedd80b
Naba Das <nabakr.das@Nabas-Mac-mini.local>
2022-06-16 12:07:25 +0530

    Live Reload in Golang Web App

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,7 @@ mkdir -p /go/myapp
 cd /go/myapp
 go mod init myapp
 go get github.com/kataras/iris/v12@master
+go get github.com/codegangsta/gin
 cp -a /app/main.go /go/myapp/main.go &>/dev/null
 chown -R {USER_ID}:{GROUP_ID} /go &>/dev/null
-go run main.go
+gin -p 8080 run main.go
--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "github.com/kataras/iris/v12"
 func main () {
 	app := iris.New()
 	app.Get("/",index)
-	app.Listen(":8080")
+	app.Listen(":3001")
 }
 
 func index(ctx iris.Context) {
